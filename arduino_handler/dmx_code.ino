709cf78f16b4a767020235f5fabe80ba9d2dd249
Alumia <alumiamusic@gmail.com>
2023-08-04 15:03:50 +0100

    +arduino code

---
+++ arduino_handler/dmx_code.ino
@@ -0,0 +1,26 @@
+#include <DmxSimple.h>
+
+void setup() {
+  Serial.begin(9600);
+  DmxSimple.maxChannel(28);
+}
+
+int value = 0;
+int channel;
+
+void loop() {
+  int c;
+
+  while(!Serial.available());
+  c = Serial.read();
+  if ((c>='0') && (c<='9')) {
+    value = 10*value + c - '0';
+  } else {
+    if (c=='c') channel = value;
+    else if (c=='w') {
+      DmxSimple.write(channel, value);
+      Serial.println();
+    }
+    value = 0;
+  }
+}
